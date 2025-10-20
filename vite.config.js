9690b840db54f0a7757f48092523d4d7a8397b8b
Pete Du Beau <peterdubeau@yahoo.com>
2025-10-19 22:35:32 -0400

    updated deployments

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/counteract_calculator/', // GitHub Pages base path
   optimizeDeps: {
     exclude: ['js-big-decimal']
   },
